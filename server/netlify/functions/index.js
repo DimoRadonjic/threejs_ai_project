7ff59ef2f69289431ae3fd2abb49ef7d21a3d703
Dimitrije Radonjic <dimo.radonjic2112@gmail.com>
2023-07-15 01:42:42 +0200

    Routes changed

---
+++ server/netlify/functions/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import serverless from 'serverless-http';
+
+import dalleRoutes from './dalle.routes.js';
+
+dotenv.config();
+
+const app = express();
+// Create a router to handle routes
+const router = express.Router();
+app.use(cors());
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use('/.netlify/functions/index/api', dalleRoutes);
+
+// Export the app and the serverless function
+export const handler = serverless(app);
+
+// app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from DALL.E' });
+});
+
+app.listen(8080, () => console.log('Server has started on port 8080'));
